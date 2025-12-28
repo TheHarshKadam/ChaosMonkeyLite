24bbec6e9777c325bc896d2a77b1958970868973
TheHarshKadam <harshvardhankadam89@gmail.com>
2025-12-28 20:56:34 +0530

    added routes and cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
-//const testRoutes = require('./routes/test.routes.js');
-const chaosRoutes = require('./routes/chaos.routes.js');
+
+app.use(cors());
 
 app.use(express.json());
-app.use('/chaos', chaosRoutes);
 
+app.use(express.static('public'));
+
+const testRoutes = require('./routes/test.routes.js');
+const chaosRoutes = require('./routes/chaos.routes.js');
+
+app.use('/test', testRoutes);
+app.use('/chaos', chaosRoutes);
 
 module.exports = app;
