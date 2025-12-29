5a21647bfbc8877360b8e5b7278b1d43b6dd92ba
TheHarshKadam <harshvardhankadam89@gmail.com>
2025-12-30 00:28:52 +0530

    added proxy route

--- src/app.js
+++ src/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({exposedHeaders: ['X-Chaos-Type']}));
-
+app.use(cors({
+    exposedHeaders: ['X-Chaos-Type']
+}));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 const testRoutes = require('./routes/test.routes.js');
 const chaosRoutes = require('./routes/chaos.routes.js');
+const proxyRoutes = require('./routes/proxy.routes.js');
 
 app.use('/test', testRoutes);
 app.use('/chaos', chaosRoutes);
+app.use('/proxy', proxyRoutes);
 
 module.exports = app;
